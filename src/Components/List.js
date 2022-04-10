0255b92290b8d9a2a29902814a98e78e2147eb1e
Vinicius <viniciusiandrade@hotmail.com>
2022-04-10 20:57:45 -0300

    Adding elements in list

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './List.css'
 
 function List() {
-  return (
-    <div className='card'>
-        <h1> My list </h1>
-        <div className='wrapper-ipt'>
-            <input type="text" placeholder='Type...' className='ipt-box'/>
-            <button className='btn-add'> + </button>
+    const [list, setList] = useState([]);
+    const [item, setItem] = useState("");
+
+    const AddToList = () => {
+        setList([...list, item]);
+        setItem("");
+    }
+
+    const RemoveFromList = () => {
+        console.log('Removed')
+    }
+    
+    return (
+        <div className='card'>
+            <h1> My list </h1>
+            <div className='wrapper-ipt'>
+                <input type="text" 
+                       value={item}
+                       placeholder='Type...' 
+                       className='ipt-box' 
+                       onChange={e => setItem(e.target.value)}/>
+                       
+                <button className='btn-add' onClick={() => AddToList()}> + </button>
+            </div>
+            <div className='wrapper-items'>
+                <ul>
+                    {list.map((list, index) => 
+                        <>
+                            <li key={index}> {list} </li>
+                            <button onClick={RemoveFromList}> - </button>
+                        </>
+                    )}
+                </ul>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default List
